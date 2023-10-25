e2919ff591b5c506ff3548415b5b787d7b3db471
Ntsay Zacarias <up202008863@fe.up.pt>
2023-10-25 07:55:37 +0000

    Update create.sql

--- src/db/create.sql
+++ src/db/create.sql
@@ -1,23 +1,51 @@
-SET search_path TO lbaw2382;
-
+-- users table
 CREATE TABLE users (
     userID SERIAL PRIMARY KEY,
-    username VARCHAR(255) NOT NULL,
-    password VARCHAR(255) NOT NULL, -- should be hashed
-    email VARCHAR(255) UNIQUE,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    name VARCHAR(255) NOT NULL,
+    username VARCHAR(255) NOT NULL UNIQUE,
+    date_of_birth DATE,
     reputation INT DEFAULT 0
 );
 
-
+-- admins table
 CREATE TABLE admins (
-    adminID SERIAL PRIMARY KEY,
-    username VARCHAR(255) NOT NULL,
-    password VARCHAR(255) NOT NULL -- should be hashed
+    adminID SERIAL PRIMARY KEY
 );
 
+-- notifications table
+CREATE TABLE notifications (
+    notificationID SERIAL PRIMARY KEY,
+    userID INT REFERENCES users(userID),
+    text VARCHAR(255) NOT NULL,
+    created_at DATE,
+    is_read BOOLEAN DEFAULT FALSE,
+    postID INT,
+    commentID INT
+);
 
+-- user_relationships table
+CREATE TABLE user_relationships (
+    relationshipID SERIAL PRIMARY KEY,
+    userID1 INT REFERENCES users(userID),
+    userID2 INT REFERENCES users(userID),
+    relationship_type VARCHAR(255),
+    UNIQUE(userID1, userID2)
+);
 
+-- comments table
+CREATE TABLE comments (
+    commentID SERIAL PRIMARY KEY,
+    postID INT,
+    authorID INT REFERENCES users(userID),
+    parent_commentID INT REFERENCES comments(commentID),
+    text VARCHAR(255) NOT NULL,
+    upvotes INT DEFAULT 0,
+    downvotes INT DEFAULT 0
+);
 
+-- posts table
 CREATE TABLE posts (
     postID SERIAL PRIMARY KEY,
     title VARCHAR(255) NOT NULL,
@@ -25,48 +53,57 @@ CREATE TABLE posts (
     authorID INT REFERENCES users(userID),
     upvotes INT DEFAULT 0,
     downvotes INT DEFAULT 0,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    created_at DATE
 );
 
+-- adding the ref for comments to posts after the posts table is defined (to avoid circular references)
+ALTER TABLE comments ADD FOREIGN KEY (postID) REFERENCES posts(postID);
 
-CREATE TABLE comments (
-    commentID SERIAL PRIMARY KEY,
-    postID INT REFERENCES posts(postID),
-    authorID INT REFERENCES users(userID),
-    parent_commentID INT REFERENCES comments(commentID),
-    text TEXT NOT NULL
-);
-
-
-CREATE TABLE tags (
-    tagID SERIAL PRIMARY KEY,
-    name VARCHAR(255) UNIQUE NOT NULL
-);
-
-
+-- categories table
 CREATE TABLE categories (
     categoryID SERIAL PRIMARY KEY,
-    name VARCHAR(255) UNIQUE NOT NULL
+    name VARCHAR(255) NOT NULL UNIQUE
 );
 
-
-CREATE TABLE reports (
-    reportID SERIAL PRIMARY KEY,
-    name TEXT NOT NULL
+-- tags table
+CREATE TABLE tags (
+    tagID SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL UNIQUE
 );
 
---  M-M relationship between posts and tags
-CREATE TABLE post_tags (
+-- category_posts (junction table)
+CREATE TABLE category_posts (
+    categoryID INT REFERENCES categories(categoryID),
+    postID INT REFERENCES posts(postID),
+    PRIMARY KEY (categoryID, postID)
+);
+
+--  (junction table)
+CREATE TABLE posts_tags (
     postID INT REFERENCES posts(postID),
     tagID INT REFERENCES tags(tagID),
     PRIMARY KEY (postID, tagID)
 );
 
---  M-M relationship between posts and categories
+-- (junction table)
 CREATE TABLE category_tags (
     categoryID INT REFERENCES categories(categoryID),
     tagID INT REFERENCES tags(tagID),
     PRIMARY KEY (categoryID, tagID)
 );
 
+-- reports table
+CREATE TABLE reports (
+    reportID SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL
+);
 
+-- blocks table
+CREATE TABLE blocks (
+    blockID SERIAL PRIMARY KEY,
+    adminID INT REFERENCES admins(adminID),
+    userID INT REFERENCES users(userID),
+    blocked_at DATE,
+    reason VARCHAR(255),
+    UNIQUE(adminID, userID)
+);
