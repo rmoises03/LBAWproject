7e24f2828cebc6a80bf7008d094bc4298370a3a4
Rodrigo Ribeiro <up202108679@fe.up.pt>
2023-11-30 16:55:40 +0000

    about us

---
+++ resources/views/about.blade.php
@@ -0,0 +1,34 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="mission">
+        <h2>Our Mission</h2>
+        <p>At NEWS4U, we believe in the power of collective knowledge. Our mission is to bring people together to share, discover, and discuss the latest news happening around the world. We're not just a news platform; we're a community-driven space where diverse voices converge to shape the narrative.</p>
+    </div>
+
+    <div class="ecosystem">
+        <h2>Collaborative News Ecosystem</h2>
+        <p>Imagine a space where journalists, experts, and everyday enthusiasts collaborate to curate and discuss news stories in real-time. NEWS4U is that space. We empower our users to be both consumers and contributors, fostering a dynamic ecosystem where news is not just consumed but actively shaped by the community.</p>
+    </div>
+
+    <div class="features">
+        <h2>Main Features</h2>
+        <ul>
+            <li>Feature 1</li>
+            <li>Feature 2</li>
+            <li>Feature 3</li>
+        </ul>
+    </div>
+
+    <div class="join">
+        <h2>Join the Conversation</h2>
+        <p>Whether you're a seasoned journalist, a passionate news follower, or someone with a unique perspective to share, NEWS4U is your platform. Join us in building a community where news is not just something you read but something you actively participate in.</p>
+        <p>Let's shape the future of news together!</p>
+    </div>
+
+    <div class="contacts">
+        <h2>Contact Us</h2>
+        <p>Email: info@example.com</p>
+        <p>Phone: +123 456 7890</p>
+    </div>
+@endsection
